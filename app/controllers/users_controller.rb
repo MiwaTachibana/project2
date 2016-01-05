deee11a0b23e37fa2bc1653a44ba8a1d580b92a1
miwatachibana <tachibam@gmail.com>
2016-01-05 15:11:04 -0800

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.order('id ASC')
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+end
+
+private
+    def user_params
+     params.require(:user).permit( :email, :password, :password_confirmation)
+    end
+end
+
