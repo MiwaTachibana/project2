0c530529d7fe621fde4c2da4458ecd203fb21480
miwatachibana <tachibam@gmail.com>
2016-01-07 12:42:01 -0800

    users can only edit/delete their own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,14 +11,16 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
+    # @post = Post.new
   end
 
   def edit
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
+    # @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
@@ -29,24 +34,48 @@ class PostsController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html {redirect_to @post, notice: 'Post was successfully updated.'}
-        format.json {render :show, status: :ok, location: @post}
-      else
-        format.html {rendr :edit}
-        format.json {render json: @post.errors, status: :unprocessable_entity}
-      end
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
+  #   respond_to do |format|
+  #     if @post.update(post_params)
+  #       format.html {redirect_to @post, notice: 'Post was successfully updated.'}
+  #       format.json {render :show, status: :ok, location: @post}
+  #     else
+  #       format.html {rendr :edit}
+  #       format.json {render json: @post.errors, status: :unprocessable_entity}
+  #     end
+  #   end
+  # end
 
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html {redirect_to posts_url, notice: 'Post was successfully destroyed.'}
-      format.json {head :no_content}
-  end
+    redirect_to posts_url
+    
+  #   respond_to do |format|
+  #     format.html {redirect_to posts_url, notice: 'Post was successfully destroyed.'}
+  #     format.json {head :no_content}
+  # end
 end
+
+
+
+
+def authenticate
+    unless logged_in?
+        flash[:error] = "You must be logged in to access this section of the site"
+        redirect_to login_form_url
+      end
+  end
+
+  def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to posts_path, notice: "You are not authorized to edit this post" if @post.nil?
+  end
+
 
 private
   def set_post
